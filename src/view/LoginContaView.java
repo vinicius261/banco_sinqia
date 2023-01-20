b181ef47c73bc7a961c638023022a5a9e0d659db
Rodolfo Lisboa <rodolfoassislisboa@gmail.com>
2023-01-20 14:38:52 -0300

    Criado pacote exceções, LoginController e início LoginContaView

--- src/view/LoginContaView.java
+++ src/view/LoginContaView.java
@@ -1,12 +1,25 @@
 package view;
 
+import model.ListaDeClientes;
+
+import java.util.Scanner;
+
 public class LoginContaView {
 
     //completar classe-metodo - coloquei um sout pra ver se funcionava
     //seria bom settar um userlogado - para facilitar pegar as infos da Conta em outras classes/metodos
     //coloquei atributo userLogado em Banco
+
+    private String clientEmail, clientPassword;
+
+    public static Scanner entrance = new Scanner(System.in);
     public void loginConta(){
-        System.out.println("logando");
+        System.out.println("Olá, para entrar no banco, por favor digite seu e-mail cadastrado:");
+        clientEmail = entrance.nextLine();
+
+        System.out.println("Por favor, agora digite sua senha:");
+        clientPassword = entrance.nextLine();
+
         MenuContaView menuContaView = new MenuContaView();
         menuContaView.mostrarMenuConta();
     }
