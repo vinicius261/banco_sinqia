2ffd5e302a188713c5ac446e052eec5baec2df12
MateusACB <mateus.a.c.b@gmail.com>
2023-01-23 23:09:10 -0300

    - Criação do método rendimento na classe DepositaView. O método calcula o valor do rendimento, envia para o DepositaController depositar e informa para o cliente que o rendimento está disponível na conta. - Criação dos métodos depositoDeslogadoView e depositoLogadoView. O cliente deslogado poderá depositar para qualquer conta. O cliente logado só poderá depositar na própria conta.

--- src/view/DepositoView.java
+++ src/view/DepositoView.java
@@ -1,29 +1,54 @@
 package view;
 
 import controller.DepositoController;
+import enums.TipoDeCliente;
+import enums.TipoDeConta;
 import model.Conta;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static database.BancoDeDados.getContas;
+import static database.BancoDeDados.getContaLogada;
 
 public class DepositoView {
     static final Scanner scan = new Scanner(System.in);
     DepositoController depositoController = new DepositoController();
     MenuInicialView menuInicialView = new MenuInicialView();
 
-    public void depositoView() {
+    public void depositoDeslogadoView() {
         System.out.println();
-        System.out.println("Bem vindo a area de depósito. Digite 1 para fazer um depósito ou digite 0 para voltar ao menu anterior.");
+        System.out.println("Digite 1 para fazer um depósito.");
+        System.out.println("Para voltar ao menu anterior digite 0.");
         System.out.println();
         String opcao = scan.next();
         if (opcao.equals("1")) {
             valorDeposito();
+            contaDeposito();
+            menuInicialView.mostrarMenuInicial();
         } else if (opcao.equals("0")) {
             menuInicialView.mostrarMenuInicial();
         } else {
-            depositoView();
+            depositoDeslogadoView();
+        }
+    }
+
+    public void depositoLogadoView() {
+        System.out.println();
+        System.out.println("Digite 1 para fazer um depósito na sua conta.");
+        System.out.println("Para voltar ao menu anterior digite 0.");
+        System.out.println();
+        String opcao = scan.next();
+        numeroConta = getContaLogada().getNumeroConta();
+        if (opcao.equals("1")) {
+            valorDeposito();
+            confirmaDeposito();
+            MenuContaView menuContaView = new MenuContaView();
+            menuContaView.mostrarMenuConta();
+        } else if (opcao.equals("0")) {
+            menuInicialView.mostrarMenuInicial();
+        } else {
+            depositoDeslogadoView();
         }
     }
 
@@ -33,7 +58,8 @@ public class DepositoView {
 
     public void valorDeposito() {
         System.out.println();
-        System.out.println("Por favor, digite o valor que gostaria de depositar ou digite 0 para retornar ao menu anterior.");
+        System.out.println("Digite o valor que gostaria de depositar.");
+        System.out.println("Para voltar ao menu anterior digite 0.");
         System.out.println();
         try {
             valorRecebido = scan.nextDouble();
@@ -44,11 +70,10 @@ public class DepositoView {
 
         boolean verificaValor = depositoController.verificaValor(valorRecebido);
         if (valorRecebido == 0) {
-            depositoView();
+            depositoDeslogadoView();
         } else {
             if (verificaValor) {
                 valorDeposito = valorRecebido;
-                contaDeposito();
             } else {
                 System.out.println("Necessario depositar um valor maior que zero.");
                 valorDeposito();
@@ -59,7 +84,8 @@ public class DepositoView {
 
     public void contaDeposito() {
         System.out.println();
-        System.out.println("Por favor, digite o número da conta ou digite 0 para retornar ao menu anterior.");
+        System.out.println("Digite o número da conta beneficiária.");
+        System.out.println("Para voltar ao menu anterior digite 0.");
         System.out.println();
         numeroConta = scan.next();
         boolean verificaConta = depositoController.verificaConta(numeroConta);
@@ -76,7 +102,7 @@ public class DepositoView {
         }
     }
 
-    public void confirmaDeposito(){
+    public void confirmaDeposito() {
         int i = depositoController.retornaPosicaoNoArray(numeroConta);
         Conta conta = getContas().get(i);
         System.out.println();
@@ -86,7 +112,8 @@ public class DepositoView {
         System.out.println("Nome do beneficiário: " + conta.getCliente().getNome());
         System.out.println("Valor: R$" + valorDeposito);
         System.out.println();
-        System.out.println("Digite 1 para confirmar ou digite 0 para cancelar e retornar ao menu anterior");
+        System.out.println("Digite 1 para confirmar.");
+        System.out.println("Para cancelar e voltar ao menu anterior digite 0.");
         System.out.println();
         String opcao = scan.next();
         if (opcao.equals("1")) {
@@ -100,9 +127,35 @@ public class DepositoView {
             System.out.println("Nome do beneficiário: " + conta.getCliente().getNome());
             System.out.println("Valor: R$" + valorDeposito);
             System.out.println();
-            menuInicialView.mostrarMenuInicial();
+            if (conta.getTipoDeConta().equals(TipoDeConta.CONTA_CORRENTE)) {
+            } else {
+                rendimento(conta);
+            }
+
         } else {
             contaDeposito();
+        }
+    }
+
+    public void rendimento(Conta conta) {
+        if (conta.getTipoDeConta().equals(TipoDeConta.CONTA_INVESTIMENTO)) {
+            if (conta.getCliente().getTipoDeCliente().equals(TipoDeCliente.PESSOA_FISICA)) {
+                double valorRendimento = valorDeposito * 0.015;
+                depositoController.deposita(valorRendimento, numeroConta);
+                System.out.println("O seu depósito na conta investimento rendeu R$ " + valorRendimento + " .");
+                System.out.println("O rendimento já está disponível na conta.");
+            } else if (conta.getCliente().getTipoDeCliente().equals(TipoDeCliente.PESSOA_JURIDICA)) {
+                double valorRendimento = valorDeposito * 0.035;
+                depositoController.deposita(valorRendimento, numeroConta);
+                System.out.println("O seu depósito na conta investimento rendeu R$ " + valorRendimento + " .");
+                System.out.println("O rendimento já está disponível na conta.");
+            }
+        } else if (conta.getTipoDeConta().equals(TipoDeConta.CONTA_POUPANCA)) {
+            double valorRendimento = valorDeposito * 0.01;
+            depositoController.deposita(valorRendimento, numeroConta);
+            System.out.println("O seu depósito na conta poupança rendeu R$ " + valorRendimento + " .");
+            System.out.println("O rendimento já está disponível na conta.");
+            System.out.println();
         }
     }
 }
