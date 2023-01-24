a654ca2dbffc98094e350a22cae96d49b76bea28
Lucas Barreto <lucasabbarreto@hotmail.com>
2023-01-24 20:18:08 -0300

    classe AbriContaView refatorada

--- src/view/AbrirContaView.java
+++ src/view/AbrirContaView.java
@@ -1,7 +1,12 @@
 package view;
 
+import controller.AbrirContaController;
 import controller.ValidarCpfCnpj;
+import controller.ValidarSenhaController;
 import controller.VerificarSeClienteExisteController;
+import enums.TipoDeCliente;
+import enums.TipoDeConta;
+import model.Cliente;
 
 import java.util.Scanner;
 
@@ -10,59 +15,110 @@ public class AbrirContaView {
 
 
     public void abrirConta(){
-        CadastrarContaPessoFisicaView cadastrarContaPessoFisicaView = new CadastrarContaPessoFisicaView();
-        CadastrarContaPessoaJuridicaView cadastrarContaPessoaJuridicaView = new CadastrarContaPessoaJuridicaView();
-
-        System.out.println("Deseja abrir uma conta para pessoa:" +
-                "\n1 -  FISICA" +
-                "\n2 - JURIDICA");
-        try {
-            int tipoDeconta = Integer.parseInt(input.nextLine());
-            if (tipoDeconta == 1 || tipoDeconta == 2) {
-                int opcao = tipoDeconta;
-                switch (opcao) {
-                    case 1:
-                        System.out.println("-------------------------- PESSOA FISICA ---------------------------");
-                        cadastrarContaPessoFisicaView.cadastrarContaPessoFisica();
-                        break;
-
-                    case 2:
-                        System.out.println("-------------------------- PESSOA JURIDICA ---------------------------");
-                        cadastrarContaPessoaJuridicaView.cadastrarContaPessoJuridica();
-                        break;
-
-                    default:
-                        System.out.println("Opcao invalida. Tente novamente.");
-                        abrirConta();
-                }
-            } else {
-                System.out.println("Opcao invalida. Tente novamente.");
-                abrirConta();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Por favor, digite um numero.");
-            abrirConta();
-        }
-    }
-
-    public void VerificarTipoPessoa(){
-        String numeroDocumento = input.nextLine();
-
         ValidarCpfCnpj validarCpfCnpj = new ValidarCpfCnpj();
         VerificarSeClienteExisteController verificarSeClienteExisteController = new VerificarSeClienteExisteController();
 
+        TipoDeCliente tipoDeCliente = null;
+        TipoDeConta tipoDeConta;
+        String senhaEscolhida;
 
-         if(validarCpfCnpj.isCNPJ(numeroDocumento)){
-            if(verificarSeClienteExisteController.verificarSeClienteExiste(numeroDocumento)){
+        System.out.println("Escolha o tipo de conta que deseja abrir (FISICA ou JURIDICA)" +
+                "\ndigitando seu CPF ou CNPJ");
 
-            } else{
-                System.out.println("Cadastrando cliente");
-                // chamar a view para cadastrar cliente
+        String numeroDocumento = input.nextLine();
+
+        if(validarCpfCnpj.isCPF(numeroDocumento) || validarCpfCnpj.isCNPJ(numeroDocumento)){
+            if(validarCpfCnpj.isCPF(numeroDocumento)){
+                tipoDeCliente = TipoDeCliente.PESSOA_FISICA;
+            } else if(validarCpfCnpj.isCNPJ(numeroDocumento)){
+                tipoDeCliente = TipoDeCliente.PESSOA_JURIDICA;
             }
-        } else{
+        } else {
             System.out.println("Documento invalido");
             MenuInicialView menuInicialView = new MenuInicialView();
             menuInicialView.mostrarMenuInicial();
         }
+
+        if(!verificarSeClienteExisteController.verificarSeClienteExiste(numeroDocumento)) {
+            CadastrarClienteView cadastrarClienteView = new CadastrarClienteView();
+            cadastrarClienteView.cadastrarCliente(numeroDocumento, tipoDeCliente);
+        }
+
+        tipoDeConta = escolherTipoDeConta(tipoDeCliente);
+        senhaEscolhida = escolherSenha();
+
+        AbrirContaController abrirContaController = new AbrirContaController();
+        abrirContaController.abrirConta(numeroDocumento, senhaEscolhida, tipoDeConta);
+    }
+
+    public TipoDeConta escolherTipoDeConta (TipoDeCliente tipoDeCliente) {
+        TipoDeConta tipoDeConta = null;
+
+        System.out.println("Deseja abrir uma conta:" +
+                "\n1 - CORRENTE" +
+                "\n2 - POUPANCA");
+        if(tipoDeCliente.equals(TipoDeCliente.PESSOA_FISICA)){
+            System.out.println("3 - Investimento");
+        }
+
+        try {
+            int tipoDeconta = Integer.parseInt(input.nextLine());
+            if (tipoDeconta == 1 || tipoDeconta == 2 || tipoDeconta == 3) {
+
+                switch (tipoDeconta) {
+                    case 1 -> {
+                        System.out.println("-----------------------------------------------------");
+                        tipoDeConta = TipoDeConta.CONTA_CORRENTE;
+                    }
+                    case 2 -> {
+                        System.out.println("-----------------------------------------------------");
+                        tipoDeConta = TipoDeConta.CONTA_POUPANCA;
+                    }
+                    case 3 -> {
+                        if(tipoDeCliente.equals(TipoDeCliente.PESSOA_FISICA)){
+                            System.out.println("-----------------------------------------------------");
+                            tipoDeConta = TipoDeConta.CONTA_INVESTIMENTO;
+                        } else {
+                            System.out.println("Opcao invalida. Tente novamente.");
+                            escolherTipoDeConta(tipoDeCliente);
+                        }
+
+                    }
+                    default -> {
+                        System.out.println("Opcao invalida. Tente novamente.");
+                        escolherTipoDeConta(tipoDeCliente);
+                    }
+                }
+            } else {
+                System.out.println("Opcao invalida. Tente novamente.");
+                escolherTipoDeConta(tipoDeCliente);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Por favor, digite um numero.");
+            escolherTipoDeConta(tipoDeCliente);
+        }
+        return tipoDeConta;
+    }
+    public String escolherSenha() {
+
+        ValidarSenhaController validarSenhaController = new ValidarSenhaController();
+        String senhaValidada = null;
+
+        System.out.println("Digite uma senha para sua conta" +
+                "\nA senha deve conte pelo menos:" +
+                "\n8 caracteres" +
+                "\n1 letra maiuscula" +
+                "\n1 letra minuscula" +
+                "\n1 Caractere especial");
+
+        String senhaDigitada = input.nextLine();
+
+        if (!validarSenhaController.validarSenha(senhaDigitada)) {
+            System.out.println("Digite uma senha valida");
+            escolherSenha();
+        } else {
+            senhaValidada = senhaDigitada;
+        }
+        return senhaValidada;
     }
 }
--- src/view/ConsultarSaldoOutrasContasView.java
+++ src/view/ConsultarSaldoOutrasContasView.java
@@ -3,9 +3,7 @@ package view;
 import controller.ConsultarSaldoController;
 import model.Conta;
 import enums.TipoDeConta;
-
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class ConsultarSaldoOutrasContasView {
@@ -13,53 +11,59 @@ public class ConsultarSaldoOutrasContasView {
     ConsultarSaldoController consultarSaldoController = new ConsultarSaldoController();
     public void SaldoOutraConta() {
         String contaSelecionada = null;
-        String tipoDeConta = null;
         String opcao;
         double saldo = 0;
         ArrayList<Conta> contas = consultarSaldoController.SaldoOutraConta();
 
-        System.out.println("Digite o tipo da conta de que deseja consultar o saldo");
-        for(int i = 0; i < contas.size(); i++){
-            switch (contas.get(i).getTipoDeConta()) {
-                case CONTA_POUPANCA -> {
-                    contaSelecionada = "POUPANCA";
+        if(!contas.isEmpty()){
+            System.out.println("Digite o tipo da conta de que deseja consultar o saldo");
+            for(int i = 0; i < contas.size(); i++){
+                switch (contas.get(i).getTipoDeConta()) {
+                    case CONTA_POUPANCA -> {
+                        contaSelecionada = "POUPANCA";
+                    }
+                    case CONTA_CORRENTE -> {
+                        contaSelecionada = "CORRENTE";
+                    }
+                    case CONTA_INVESTIMENTO -> {
+                        contaSelecionada = "INVESTIMENTO";
+                    }
                 }
-                case CONTA_CORRENTE -> {
-                    contaSelecionada = "CORRENTE";
-                }
-                case CONTA_INVESTIMENTO -> {
-                    contaSelecionada = "INVESTIMENTO";
-                }
+                System.out.println(contaSelecionada);
             }
-            System.out.println(contaSelecionada);
-        }
-        opcao = input.nextLine().toUpperCase();
 
-        switch (opcao) {
-            case "POUPANCA" -> {
-                for(Conta conta : contas){
-                    if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_POUPANCA)){
-                        saldo = conta.getSaldo();
+            opcao = input.nextLine().toUpperCase();
+
+            switch (opcao) {
+                case "POUPANCA" -> {
+                    for(Conta conta : contas){
+                        if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_POUPANCA)){
+                            saldo = conta.getSaldo();
+                        }
                     }
+                    System.out.println("O saldo da sua conta poupanca e de: " + saldo);
                 }
-                System.out.println("O saldo da sua conta poupanca e de: " + saldo);
-            }
-            case "CORRENTE" -> {
-                for(Conta conta : contas){
-                    if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_CORRENTE)){
-                        saldo = conta.getSaldo();
+                case "CORRENTE" -> {
+                    for(Conta conta : contas){
+                        if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_CORRENTE)){
+                            saldo = conta.getSaldo();
+                        }
                     }
+                    System.out.println("O saldo da sua conta corrente e de: " + saldo);
                 }
-                System.out.println("O saldo da sua conta corrente e de: " + saldo);
-            }
-            case "INVESTIMENTO" -> {
-                for(Conta conta : contas){
-                    if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_INVESTIMENTO)){
-                        saldo = conta.getSaldo();
+                case "INVESTIMENTO" -> {
+                    for(Conta conta : contas){
+                        if(conta.getTipoDeConta().equals(TipoDeConta.CONTA_INVESTIMENTO)){
+                            saldo = conta.getSaldo();
+                        }
                     }
+                    System.out.println("O saldo da sua conta investimento e de: " + saldo);
                 }
-                System.out.println("O saldo da sua conta investimento e de: " + saldo);
             }
+        } else {
+            System.out.println("Você não possui outra(s) conta(s).");
         }
+
+
     }
 }
